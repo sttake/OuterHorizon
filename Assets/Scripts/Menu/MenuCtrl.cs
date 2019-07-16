b1bf7654f6695216f3995897238ff07f968fed56
kimura naritaka <kimunii1025@gmail.com>
2019-07-16 08:54:23 +0000

    No Comments

---
+++ Assets/Scripts/Menu/MenuCtrl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCtrl : MonoBehaviour
+{
+    public GameObject MenuWindow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MenuWindow.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("Escape"))
+        {
+            MenuWindow.SetActive(true);
+        }
+    }
+}
