b7640a438c6bbee4e9c1c4dc2370e3911ced5fc8
watano1168 <watano1168@gmail.com>
2019-07-21 13:25:42 +0000

    ショートカット左右選択の実装

---
+++ Assets/Scripts/UI/ShortCut/ShortCuts.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShortCuts : MonoBehaviour {
+
+    [SerializeField] private int currentItem = 0;
+    [SerializeField] private Animator[] shortcutItems = null;
+    private int prevItem;
+    private const int shortcutMax = 10;
+    private float prevInput = 0;
+    private float currentInput = 0;
+
+    void Start() {
+        shortcutItems[currentItem].SetBool("isActive", true);
+    }
+
+    void Update() {
+        GetInput();
+        ChangeActiveItem();
+    }
+
+    void GetInput() {
+        currentInput = 0;
+        var input = Input.GetAxisRaw("ShortCutLR");
+        if (prevInput != input) currentInput = input;
+        prevInput = input;
+    }
+
+    void ChangeActiveItem() {
+        if(currentInput == 0) return;
+        else if(currentInput == 1) currentItem++;
+        else currentItem += shortcutMax - 1;
+        currentItem %= shortcutMax;
+        shortcutItems[prevItem].SetBool("isActive", false);
+        shortcutItems[currentItem].SetBool("isActive", true);
+        prevItem = currentItem;
+    }
+
+}
